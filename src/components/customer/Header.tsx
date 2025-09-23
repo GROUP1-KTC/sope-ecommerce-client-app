e5e6f6a9a7bf0b00a30c935cfa5bb8bf794f9abb
sonhoang3000 <vuhoangson3000@gmail.com>
2025-09-23 19:56:49 +0700

    check product_id

--- src/components/customer/Header.tsx
+++ src/components/customer/Header.tsx
@@ -34,63 +34,66 @@ const Header = () => {
     const { data: products = [] } = useSearchSuggestQuery(
         searchTerm
             ? {
-                  _source: ['product_id', 'slug', 'name', 'default_image'],
-                  query: {
-                      function_score: {
-                          query: {
-                              bool: {
-                                  should: [
-                                      {
-                                          match_phrase: {
-                                              name: {
-                                                  query: searchTerm,
-                                                  boost: 5,
-                                              },
-                                          },
-                                      },
-                                      {
-                                          match_phrase_prefix: {
-                                              name: {
-                                                  query: searchTerm,
-                                                  boost: 4,
-                                              },
-                                          },
-                                      },
-                                      {
-                                          match: {
-                                              name: {
-                                                  query: searchTerm,
-                                                  fuzziness: 'AUTO',
-                                                  boost: 2,
-                                              },
-                                          },
-                                      },
-                                      {
-                                          match: {
-                                              slug: {
-                                                  query: searchTerm,
-                                                  boost: 1,
-                                              },
-                                          },
-                                      },
-                                  ],
-                              },
-                          },
-                          boost_mode: 'sum',
-                      },
-                  },
-                  size: 20,
-                  sort: [{ _score: 'desc' }],
-              }
+                _source: ['product_id', 'slug', 'name', 'default_image'],
+                query: {
+                    function_score: {
+                        query: {
+                            bool: {
+                                should: [
+                                    {
+                                        match_phrase: {
+                                            name: {
+                                                query: searchTerm,
+                                                boost: 5,
+                                            },
+                                        },
+                                    },
+                                    {
+                                        match_phrase_prefix: {
+                                            name: {
+                                                query: searchTerm,
+                                                boost: 4,
+                                            },
+                                        },
+                                    },
+                                    {
+                                        match: {
+                                            name: {
+                                                query: searchTerm,
+                                                fuzziness: 'AUTO',
+                                                boost: 2,
+                                            },
+                                        },
+                                    },
+                                    {
+                                        match: {
+                                            slug: {
+                                                query: searchTerm,
+                                                boost: 1,
+                                            },
+                                        },
+                                    },
+                                ],
+                            },
+                        },
+                        boost_mode: 'sum',
+                    },
+                },
+                size: 20,
+                sort: [{ _score: 'desc' }],
+            }
             : skipToken,
     );
 
+    console.log('check products', products);
+
+
     const [logClick] = useLogProductClickMutation();
 
-    const handleClickProduct = (productId: string, keyword: string) => {
-        console.log('check productId', productId);
+    const handleClickProduct = (product_id: string, keyword: string) => {
+        console.log('check product_id', product_id);
         logClick({
-            product_id: productId,
+            product_id: product_id,
             keyword,
             timestamp: new Date().toISOString(),
         });
@@ -285,9 +288,8 @@ const Header = () => {
             )}
 
             <div
-                className={`fixed top-0 right-0 h-full w-3/4 max-w-[300px] bg-[#d0001a] text-white z-50 transform transition-transform duration-300 ease-in-out ${
-                    menuOpen ? 'translate-x-0' : 'translate-x-full'
-                } sm:hidden flex flex-col p-5 gap-3 rounded-l-xl shadow-lg`}
+                className={`fixed top-0 right-0 h-full w-3/4 max-w-[300px] bg-[#d0001a] text-white z-50 transform transition-transform duration-300 ease-in-out ${menuOpen ? 'translate-x-0' : 'translate-x-full'
+                    } sm:hidden flex flex-col p-5 gap-3 rounded-l-xl shadow-lg`}
             >
                 <div className="flex justify-end">
                     <button
